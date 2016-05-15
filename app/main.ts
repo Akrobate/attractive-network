22fe5615aad47e85274587c606c331f4f6c39b68
Artiom FEDOROV <artiom@localhost.localdomain>
2016-05-15 12:10:28 +0200

    test prod mode

--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,7 @@
 import {bootstrap}    from '@angular/platform-browser-dynamic';
 import {AppComponent} from './app.component';
 import {Application} from './app.application';
-
+import {enableProdMode} from '@angular/core';
+enableProdMode();
 
 bootstrap(Application);
